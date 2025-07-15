9a6aff004109c012c3a8e9e29e24bcd2244b201c
Dartagnan <solucoesconsultoria542@gmail.com>
2025-07-15 08:36:11 -0300

    set up project, Input component, ChatMessage Component, icos, and initial styles

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,43 @@
+function ChatMessage(props) {
+  const { message, sender } = props;
+  const isUser = sender === "user";
+
+  const time = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return (
+    <div
+      className={`mt-4 flex ${isUser ? "justify-end" : "justify-start"} w-full`}
+    >
+      <div
+        className={`max-w-[70%] flex flex-col items-${
+          isUser ? "end" : "start"
+        } gap-1`}
+      >
+        <div
+          className={`flex items-end gap-2 ${isUser ? "flex-row-reverse" : ""}`}
+        >
+          <img
+            src={isUser ? "/user.svg" : "/bot.svg"}
+            alt={isUser ? "user-icon" : "bot-icon"}
+            className="w-10 h-10 rounded-full shadow-md"
+          />
+          <p
+            className={`p-3 rounded-2xl text-sm shadow-md leading-relaxed ${
+              isUser
+                ? "bg-blue-600 text-white rounded-br-none"
+                : "bg-gray-200 text-gray-900 rounded-bl-none"
+            }`}
+          >
+            {message}
+          </p>
+        </div>
+        <span className="text-xs text-gray-100">{time}</span>
+      </div>
+    </div>
+  );
+}
+
+export default ChatMessage;
